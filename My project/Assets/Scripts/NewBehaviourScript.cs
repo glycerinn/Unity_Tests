95522eaa6a00f28c0d36c7b0ab006399444c27fb
glycerinn <glynisdurdin07@gmail.com>
2025-06-10 17:57:26 +0700

    added groundcheck

--- My project/Assets/Scripts/NewBehaviourScript.cs
+++ My project/Assets/Scripts/NewBehaviourScript.cs
@@ -1,22 +1,20 @@
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     public Rigidbody2D myrigidbody;
-    public float jump;
+    public bool grounded;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
-        {
-            myrigidbody.linearVelocity = Vector2.up * 10;
-        }
+        
         if (Input.GetKeyDown(KeyCode.LeftArrow) == true)
         {
             myrigidbody.linearVelocity = Vector2.left * 10;
@@ -25,5 +23,18 @@ public class NewBehaviourScript : MonoBehaviour
         {
             myrigidbody.linearVelocity = Vector2.right * 10;
         }
+
+        if (grounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) == true)
+            {
+            myrigidbody.linearVelocity = Vector2.up * 10;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.CircleCast(transform.position, 2.45f, Vector2.down, 0.05f);
     }
 }
